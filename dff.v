b2e387df806ce28761f22a8e6b26d5e64d75d567
Kadi Chandu <chandukadi111@gmail.com>
2025-08-04 20:12:06 +0530

    Update dff.v

--- dff.v
+++ dff.v
@@ -1,17 +1,12 @@
 module dff(
   input d ,
-  input clk , 
+  input clk,
   output q , 
-  output qbar);
-  wire nd , d_and_clk , nd_and_clk;
-  wire s , r ;
-  wire q_int , qbar_int ;
-  not(nd , d); // inverting d 
-  and (d_and_clk , d , clk ); // generating set signal
-  and (nd_and_clk , nd , clk ); // generating reset signal
-  nand (q_int , d_and_clk , qbar_int); // s input of sr latch
-  nand (qbar_int , nd_and_clk , q_int); // r input of sr latch
-  buf (q , q_int);
-  buf (qbar , qbar_int);
+  output q_bar );
+  wire x , y ;
+  nand A (x , d , clk);
+  nand B (y , x , clk);
+  nand C (q  , q_bar , x);
+  nand D (q_bar , q , y);
 endmodule
   
