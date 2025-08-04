755808584703b3a69a983540666c18d476055e2e
Kadi Chandu <chandukadi111@gmail.com>
2025-08-04 20:21:01 +0530

    Create dff_tb.v

---
+++ dff_tb.v
@@ -0,0 +1,18 @@
+module dff_tb;
+    reg d, clk;
+    wire q, q_bar;
+    dff uut (
+        .d(d),
+        .clk(clk),
+        .q(q),
+        .q_bar(q_bar);
+    always #5 CLK = ~CLK;
+    initial begin
+        clk = 0; d = 0; #10;
+        d = 1; #10;
+        d = 0; #10;
+        d = 1; #10;
+        d = 1; #10;
+        $finish;
+    end
+endmodule
